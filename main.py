25e181d625e4ff6ea85617b96ba83820ff2cf149
ChaseRosipayla <115881505+ChaseRosipayla@users.noreply.github.com>
2022-10-29 15:01:00 -0400

    Fixed the errors, used more bs4

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 import cloudscraper
-import re
-import json
-from colorama import Fore
-scraper = cloudscraper.create_scraper(
-    browser={
-        'browser': 'chrome',
-        'platform': 'android',
-        'desktop': False
-    }
-)
-def get_results(url):
-    response = scraper.get(url)
-    #print(Fore.GREEN,"Test 1: Passed")
-    return(response.text)
-def parse_page(results):
-    splitt = results.split("window.Quizlet")
-    split = splitt[6].split("AdHeading AdHeading--right")
-    call = split[0].split("QLoad")
-    result = call[0].strip(";")
-    result = result.strip('["MiniFlashcards"] = ')
-    result = result.strip(';')
-    results_json = json.loads(result)
-    data = (results_json.get('studiableDocumentData'))
-    suitable_items = data.get('studiableItems')
-    return(suitable_items)
-link = input("Enter quizlet link:   ")
-mode = input("Choose a mode: [>>,<>,<<]")
-results = (get_results(url=link))
-suitable_items = parse_page(results=results)
-for n in range(len(suitable_items)):
-    card_sides = suitable_items[n].get('cardSides') ### CHANGE TO MOVE TO NEXT SLIDE
-    question_side = card_sides[0].get('media')
-    def_side = card_sides[1].get('media')
-    question = (question_side[0].get('plainText'))
-    answer = (def_side[0].get('plainText'))
-    print(Fore.RESET,f"{question}{mode}{answer}")
+scraper = cloudscraper.create_scraper()
+link = input("Quizlet Link")
+method = input("Choose a method, <<, >> , <>")
+response = scraper.get(url=link)
+#print(response.text)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+div = soup.findAll('div', {'class': 'SetPageTerm-sideContent'})
+for n in range(len(div)):
+    t1 = div[n].text
+    try:
+        t2 = div[n+1].text
+    except:
+        break
+    print(f'{t1}{method}{t2}')
+    #print(div.findAll('div', {'class': 'SetPageTerm-sideContent'}))
+    #print(str(div).strip('<div class="SetPageTerm-sideContent"><a class="SetPageTerm-wordText"><span class='))
+#print(soup.find("TermText notranslate lang-en").text)
+#for link in soup.findAll('class="TermText notranslate lang-en"'):
+   # print(link)
